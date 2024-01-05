b5914c76d4178de3ea5a5dd5465c0e3d60393abf
jpelegrino1010 <julio.pelegrino@viseo.com>
2024-01-05 11:11:18 -0400

    Project setup

---
+++ cenord-layered/src/main/java/com/julioluis/cenordlayered/CenordLayeredApplication.java
@@ -0,0 +1,13 @@
+package com.julioluis.cenordlayered;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CenordLayeredApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CenordLayeredApplication.class, args);
+    }
+
+}
---
+++ cenord-layered/src/test/java/com/julioluis/cenordlayered/CenordLayeredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julioluis.cenordlayered;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CenordLayeredApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
